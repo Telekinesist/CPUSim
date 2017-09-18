60c788ba531521df2af7d63eaeb41f463bba7693
Bjørn Kjær <bjoernak@gmail.com>
2017-09-18 21:47:46 +0200

    Finished RAM and register textbox generation. Added most operators. Bitshift is the only operation confirmed to be broken. Added formatting and sanitizing event handlers to the textboxes for easayer typing, and to prevent illigal inputs. XAML code cleanup. Window element access workaround by setting variables in other classes as references initialiced by the Window partial class.

--- CPUSim/MainWindow.xaml.cs
+++ CPUSim/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,234 @@ namespace CPUSim
         public MainWindow()
         {
             InitializeComponent();
+            BuildRegisters();
+            BuildRAM();
+            Sim.SetRAM(RAM);
+            Sim.Setreg(Registers);
+        }
+
+        public Int16 GetMem(string addr)
+        {
+            TextBox cell = (TextBox)FindName("M" + addr);
+            Int16 value = Int16.Parse(cell.Text, System.Globalization.NumberStyles.HexNumber);
+            return value;
+        }
+
+        public void SetMem(string addr, string value)
+        {
+            TextBox cell = (TextBox)this.FindName("M" + addr);
+            cell.Text = value;
+        }
+
+        public Int16 GetReg(string addr)
+        {
+            TextBox cell = (TextBox)this.FindName("R" + addr);
+            Int16 value = Int16.Parse(cell.Text, System.Globalization.NumberStyles.HexNumber);
+            return value;
+        }
+
+        public void SetReg(string addr, string value)
+        {
+            TextBox cell = (TextBox)this.FindName("R" + addr);
+            cell.Text = value;
+        }
+
+        void BuildRegisters()
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                RowDefinition row = new RowDefinition()
+                {
+                    Height = new GridLength(18)
+                };
+                Registers.RowDefinitions.Add(row);
+
+                StackPanel st = new StackPanel()
+                {
+                    Orientation = Orientation.Horizontal,
+                    Margin = new Thickness(5, 0, 0, 0)
+                };
+                Grid.SetColumn(st, 0);
+                Grid.SetRow(st, i);
+
+                TextBlock name = new TextBlock
+                {
+                    Text = i.ToString("X"),
+                    Width = 25,
+                    TextAlignment = TextAlignment.Center,
+                };
+                st.Children.Add(name);
+
+                TextBox tb = new TextBox
+                {
+                    Name = "R" + i.ToString(),
+                    Width = 50,
+                    Text = "00",
+                    
+                };
+                tb.TextChanged += new TextChangedEventHandler(TextBox_TextChanged);
+                tb.LostFocus += new RoutedEventHandler(TextBox_LostFocus);
+                tb.GotFocus += new RoutedEventHandler(TextBox_GotFocus);
+                st.Children.Add(tb);
+
+                Registers.Children.Add(st);
+
+            }
+            RowDefinition space = new RowDefinition() //Adds a space between PC and the other registers
+            {
+                Height = new GridLength(10),
+            };
+            RowDefinition PC = new RowDefinition()
+            {
+                Height = new GridLength(20),
+            };
+            Registers.RowDefinitions.Add(space);
+            Registers.RowDefinitions.Add(PC);
+
+                                                       //Why the 2s? Because variable names. THAT is why
+            StackPanel st2 = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal,
+                Margin = new Thickness(5, 0, 0, 0)
+            };
+            Grid.SetColumn(st2, 0);
+            Grid.SetRow(st2, 17);
+
+            TextBlock name2 = new TextBlock
+            {
+                Text = "PC",
+                Width = 25,
+                TextAlignment = TextAlignment.Center,
+            };
+            st2.Children.Add(name2);
+
+            TextBox tb2 = new TextBox
+            {
+                Name = "PC",
+                Width = 50,
+                Text = "00",
+            };
+            tb2.TextChanged += new TextChangedEventHandler(TextBox_TextChanged);
+            tb2.LostFocus += new RoutedEventHandler(TextBox_LostFocus);
+            tb2.GotFocus += new RoutedEventHandler(TextBox_GotFocus);
+            st2.Children.Add(tb2);
+
+            Registers.Children.Add(st2);
+
+        }
+        void BuildRAM()
+        {
+            for (int y = 0; y < 17; y++)
+            {
+                ColumnDefinition col = new ColumnDefinition();
+                RAM.ColumnDefinitions.Add(col);
+                for (int x = 0; x < 17; x++)
+                {
+                    RowDefinition row = new RowDefinition();
+                    RAM.RowDefinitions.Add(row);
+                    WrapPanel wp = new WrapPanel()
+                    {
+                        Height = 18,
+                        Width = 25,
+                        HorizontalAlignment = HorizontalAlignment.Center
+                    };
+                    if (y == 0 && x == 0)
+                    {
+                        //Do nothing
+                    }
+                    else if (y == 0)
+                    {
+                        TextBlock tb = new TextBlock()
+                        {
+                            Width = 25,
+                            Text = (x-1).ToString("X"),
+                            TextAlignment = TextAlignment.Center
+                        };
+                        wp.Children.Add(tb);
+                    }
+                    else if (x == 0)
+                    {
+                        TextBlock tb = new TextBlock()
+                        {
+                            Height = 25,
+                            Width = 25,
+                            TextAlignment = TextAlignment.Center,
+                            Text = (y - 1).ToString("X"),
+                        };
+                        wp.Children.Add(tb);
+                    }
+                    else
+                    {
+                        TextBox tb = new TextBox()
+                        {
+                            Height = 25,
+                            Width = 25,
+                            TextAlignment = TextAlignment.Center,
+                            Text = "00",
+                            Name = "M" + x.ToString("X") + y.ToString("X"),
+                        };
+                        tb.TextChanged += new TextChangedEventHandler(TextBox_TextChanged);
+                        tb.LostFocus += new RoutedEventHandler(TextBox_LostFocus);
+                        tb.GotFocus += new RoutedEventHandler(TextBox_GotFocus);
+                        wp.Children.Add(tb);
+                    }
+                    Grid.SetRow(wp, y);
+                    Grid.SetColumn(wp, x);
+                    RAM.Children.Add(wp);
+                }
+            }
+        }
+
+        private void Step_Click(object sender, RoutedEventArgs e)
+        {
+            Sim.Step();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.SelectAll();     //Does not work well with clicking, but it is not unusable
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) //Only intented for RAM and register autoformatting while typing
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length > 2)
+            {
+                if (tb.CaretIndex == 1)
+                {
+                    tb.Text = tb.Text[0].ToString() + tb.Text[2].ToString();
+                    tb.CaretIndex = 1;
+                }
+                else if (tb.CaretIndex == 2)
+                {
+                    tb.Text = tb.Text[0].ToString() + tb.Text[1].ToString();
+                    tb.CaretIndex = 2;
+                }
+                else if (tb.CaretIndex == 3)
+                {
+                    tb.Text = tb.Text[1].ToString() + tb.Text[2].ToString();
+                    tb.CaretIndex = 2;
+                }
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e) //Only intented for RAM and register formatting. Sanitizes TextBox content
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length > 2)
+            {
+                tb.Text = tb.Text[1].ToString() + tb.Text[2].ToString();
+            }
+            else if (tb.Text.Length == 1)
+            {
+                tb.Text = "0" + tb.Text;
+            }
+            else if (tb.Text.Length == 0)
+            {
+                tb.Text = "00";
+            }
+            tb.Text = Regex.Replace(tb.Text, "[^0-9|a-f|A-F]", "0").ToUpper(); //Replaces illigal characters with 0, and makes them uppercase for consistency
         }
     }
 }
---
+++ CPUSim/Sim.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CPUSim
+{
+    static class Sim
+    {
+        public static TextBox[] RAM = new TextBox[256];     //This is one of the best ugly workarounds I've come up with
+        public static TextBox[] reg = new TextBox[16];
+        public static TextBox PC;
+
+        //These methods makes the variables above references to the TextBox Elements in the window
+        //I am genuently sorry about this
+        public static void SetRAM(Grid r)
+        {
+            int counter = 0;
+            foreach (UIElement u in r.Children)
+            {
+                if (u.GetType() == typeof(WrapPanel))
+                {
+                    WrapPanel wp = (WrapPanel)u;
+                    if (wp.Children.Count > 0)
+                    {
+                        if (wp.Children[0].GetType() == typeof(TextBox))
+                        {
+                            RAM[counter] = (TextBox)wp.Children[0];
+                            counter++;
+                        }
+                    }
+                }
+            }
+        }
+        public static void Setreg(Grid r)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                reg[i] = (TextBox)((StackPanel)r.Children[i]).Children[1];
+            }
+            PC = (TextBox)((StackPanel)r.Children[16]).Children[1]; //I know this is hacky
+        }
+
+        public static void Step()
+        {
+            int instr = int.Parse(PC.Text, System.Globalization.NumberStyles.HexNumber);
+            char opc = RAM[instr].Text[0];
+
+            string data;
+            int to, from;
+            byte num1, num2, res, arg;
+            string temp, temp1, temp2;
+
+            switch (opc)
+            {
+                case '0':   //0xxx No instruction
+                    break;
+                case '1':   //1RXY Copy content of RAM cell XY to register R
+                    data = RAM[instr + 1].Text;
+                    to = int.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    reg[to].Text = data;
+                    break;
+                case '2':   //2RXY Set value of register R to XY
+                    to = int.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    reg[to].Text = RAM[instr + 1].Text;
+                    break;
+                case '3':   //3RXY Copy content of register R to RAM cell XY
+                    to = int.Parse(RAM[instr + 1].Text, System.Globalization.NumberStyles.HexNumber);
+                    from = int.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    RAM[to].Text = reg[from].Text;
+                    break;
+                case '4':   //4xRS Copy content of register R to register S
+                    to = int.Parse(RAM[instr + 1].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    from = int.Parse(RAM[instr + 1].Text[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    reg[to].Text = reg[from].Text;
+                    break;
+                case '5':   //5RST Add together register S and T as two's compliment, and store the result in register R
+                    num1 = byte.Parse(RAM[instr + 1].Text[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(RAM[instr + 1].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num1 = byte.Parse(reg[num1].Text, System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(reg[num2].Text, System.Globalization.NumberStyles.HexNumber);
+                    to = int.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    
+                    res = (byte)(num1 + num2);
+                    reg[to].Text = res.ToString("X");
+                    break;
+                case '6':   //5RST Add together register S and T as floating point, and store the result in register R
+                    //Not implemented.
+                    break;
+                case '7':     //7RST Perform bitwise OR on register S and T, and store the result in register R
+                    num1 = byte.Parse(RAM[instr + 1].Text[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(RAM[instr + 1].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num1 = byte.Parse(reg[num1].Text, System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(reg[num2].Text, System.Globalization.NumberStyles.HexNumber);
+                    to = int.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    res = (byte)(num1 | num2);
+                    reg[to].Text = res.ToString("X");
+                    break;
+                case '8':   //8RST Perform bitwise AND on register S and T, and store the result in register R
+                    num1 = byte.Parse(RAM[instr + 1].Text[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(RAM[instr + 1].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num1 = byte.Parse(reg[num1].Text, System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(reg[num2].Text, System.Globalization.NumberStyles.HexNumber);
+                    to = int.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    res = (byte)(num1 & num2);
+                    reg[to].Text = res.ToString("X");
+                    break;
+                case '9':   //9RST Perform bitwise XOR on register S and T, and store the result in register R
+                    num1 = byte.Parse(RAM[instr + 1].Text[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(RAM[instr + 1].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num1 = byte.Parse(reg[num1].Text, System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(reg[num2].Text, System.Globalization.NumberStyles.HexNumber);
+                    to = int.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    res = (byte)(num1 ^ num2);
+                    reg[to].Text = res.ToString("X");
+                    break;
+                case 'A':   //ARxX Cyclycally rotate register R, X steps from right to left
+                    //This seems to only work once?!?!?
+                    to = byte.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num1 = byte.Parse(reg[to].Text, System.Globalization.NumberStyles.HexNumber);
+                    arg = byte.Parse(RAM[instr + 1].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    
+                    //Convert.ToString(num1, 2));
+                    temp = Convert.ToString(num1, 2);
+                    temp1 = temp.Substring(0, arg);
+                    temp2 = temp.Substring(arg);
+                    temp = temp2 + temp1;
+                    reg[to].Text = (Convert.ToByte(temp, 2)).ToString("X");
+                    
+                    break;
+                case 'B':   //BRXY Jump to instruction in RAM cell XY if register R equals the content of register 0
+                    num1 = byte.Parse(reg[0].Text, System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(reg[num2].Text, System.Globalization.NumberStyles.HexNumber);
+                    if (num1 == num2)
+                    {
+                        temp = RAM[instr + 1].Text;
+                        PC.Text = temp;
+                    }
+                    break;
+                case 'C':   //Cxxx Stops the program
+                    //Not implementet
+                    break;
+                case 'D':   ////BRXY Jump to instruction in RAM cell XY if register R is larger than register 0
+                    sbyte snum1 = sbyte.Parse(reg[0].Text, System.Globalization.NumberStyles.HexNumber);
+                    num2 = byte.Parse(RAM[instr].Text[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+                    sbyte snum2= sbyte.Parse(reg[num2].Text, System.Globalization.NumberStyles.HexNumber);
+                    if (snum1 < snum2)
+                    {
+                        temp = RAM[instr + 1].Text;
+                        PC.Text = temp;
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Invalid Opcode: " + opc);
+                    break;
+            }
+            PC.Text = (instr + 2).ToString("X");
+        }
+    }
+}
